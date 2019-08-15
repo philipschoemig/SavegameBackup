63b583c232af5e8b42e51a97c446a5f96342c83b
Philip Schömig <philip.schoemig@posteo.de>
2019-08-15 23:25:21 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='SavegameBackup',
-    version='1.7.0',
+    version='2.1.1',
     description='A software for backing up save files of computer games',
     license='BSD',
     author='Philip Schömig',
     author_email='philip.schoemig@posteo.de',
     url='https://github.com/philipschoemig/SavegameBackup/',
-    packages=[
-        'savegamebackup',
-        'savegamebackup.configuration',
-        'savegamebackup.subcommands',
-        'savegamebackup.utils'
-    ],
+    packages=find_packages(),
 )
